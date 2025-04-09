a8a51a5ab3d2d4247c210dbe4a77cca0a15ff487
aloopops <abdullahali1692011@gmail.com>
2025-04-09 19:29:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ import logging
 from flask import Flask, render_template, request, jsonify
 import g4f
 from g4f.client import Client
-from flask import make_response
 
-return make_response(jsonify({'status': 'success', 'message': ai_response}), 200)
 
 # Configure logging
 
